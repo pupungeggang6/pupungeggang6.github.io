93f61129906381953aba7cc3750cf1582b38eeb6
pupungeggang6 <pupungeggang6@gmail.com>
2025-01-16 16:49:00 +0900

    Start

--- load.js
+++ load.js
@@ -1,7 +1,14 @@
 window.onload = load
 
 function load() {
-    document.getElementById('header').innerHTML = `
-        asdf
+    document.getElementById('Header').innerHTML = `
+        <div id="Banner" onclick="location.href = 'index.html'">pupungeggang6's blog</div>
     `
+    document.getElementById('UpperBar').innerHTML = `
+    `
+}
+
+function addBack() {
+    const buttonBack = document.createElement('button')
+    document.body.appendChild(buttonBack)
 }
